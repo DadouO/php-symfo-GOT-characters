89cd7d0bc814175afa3baaac67e30067f5d8d65a
darlene.bechtel@etu.umontpellier.fr <darlene.bechtel@etu.umontpellier.fr@g7.fdsetu.infra.umontpellier.fr>
2019-11-04 12:07:52 +0100

    modify view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="accueil")
      */
-    public function index()
+    public function accueil()
     {
+        $aHello = ["Hi","Yo", "What's up", "Bien ou bien"];
+        shuffle($aHello);
+
+        $GOT_character =  
+            file_get_contents('https://anapioficeandfire.com/api/characters/583');
+            dump($GOT_character);
+
+        //vue 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'aHello' => $aHello,
+            'char' => json_decode($GOT_character)
         ]);
     }
 }
